cc6f52438ccc17adc46382c4e3fa0ac34a99fff2
jtpierson3 <jtpierson3@yahoo.com>
2020-04-15 10:03:48 -0500

    First Update

---
+++ boot2.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	setTimeout(function() {
+		$("#fenderPre").removeClass("preloader");
+		$("#loadImg").addClass("hidePreloader");
+		$("#fenderPre").addClass("hidePreloader");
+	}, 500);
+});
